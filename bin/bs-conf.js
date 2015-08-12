4a91d4d07034d2b6b9f8070c4f4e8550dca3dbe4
Per Guth <mail@perguth.de>
2015-08-12 14:38:52 +0200

    init

---
+++ bin/bs-conf.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var path = require('path')
+
+
+var browserSync = require('browser-sync')
+var conf = require(
+	path.resolve(process.cwd(), './bs.conf.js')	)
+
+browserSync(conf)
---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./bin/bs-conf.js')
